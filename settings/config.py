e1bb346c8de39c8e42b0e8ee166a4f46cb05071a
gfredtech <gfredtech@gmail.com>
2019-02-20 19:58:59 +0000

    Database

--- settings/config.py
+++ settings/config.py
@@ -7,10 +7,10 @@ LOG_FILE_NAME = 'log'
 LOG_MAX_SIZE_BYTES = 1024 * 1024  # MB
 LOGGER_NAME = 'logger'
 
-REGISTERED_COURSES = {'Lvl 100':    ['Telecom Eng.', 'Comp Eng', 'Informatics', 'IT', 'Comp Sci', 'Software Eng'],
-                      'Lvl 200':    ['Telecom Eng.', 'Comp Eng', 'Informatics', 'IT', 'Comp Sci', 'Software Eng'],
-                      'Lvl 300':    ['Telecom Eng.', 'Comp Eng', 'Informatics', 'IT', 'Comp Sci', 'Software Eng'],
-                      'Lvl 400':    ['Telecom Eng.', 'Comp Eng', 'Informatics', 'IT'],
+REGISTERED_COURSES = {'Lvl 100':    ['Telecom Eng', 'Comp Eng'],
+                      'Lvl 200':    ['Telecom Eng', 'Comp Eng'],
+                      'Lvl 300':    ['Telecom Eng', 'Comp Eng'],
+                      'Lvl 400':    ['Telecom Eng', 'Comp Eng'],
                       }
 
 
